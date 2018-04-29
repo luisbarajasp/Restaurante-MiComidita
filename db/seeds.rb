e3fad4a0db9c6b7a353439c04c0ec7d2354a40cf
Mauricio Rico <mrico005@gmail.com>
2018-04-29 12:40:01 -0500

    Seeds de las 20 recetas

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,7 +16,7 @@ RawInventory.create(quantity: 1, expired_at: '2018-05-10', raw: rice)
 recipe_materials << RecipeMaterial.create(raw: rice, quantity: 0.1)
 
 lemon = Raw.create(name: 'Lemon', type: 'fresh', measure: 'unit', cost: '5')
-RawInventory.create(quantity: 10, expired_at: '2018-05-05', raw: lemon)
+RawInventory.create(quantity: 20, expired_at: '2018-05-05', raw: lemon)
 recipe_materials << RecipeMaterial.create(raw: lemon, quantity: 1)
 
 zucchini = Raw.create(name: 'Zucchini', type: 'fresh', measure: 'kilogram', cost: '100')
@@ -154,7 +154,7 @@ cereal = Raw.create(name: 'Cereal', type: 'miscellaneous', measure: 'gram', cost
 RawInventory.create(quantity: 450, expired_at: '2018-10-03', raw: cereal)
 recipe_materials << RecipeMaterial.create(raw: cereal, quantity: 100)
 
-milk = Raw.create(name: 'Milk', type: 'fresh', measure: 'liter', cost: '340')
+milk = Raw.create(name: 'Milk', type: 'dairy', measure: 'liter', cost: '340')
 RawInventory.create(quantity: 4, expired_at: '2019-07-06', raw: milk)
 recipe_materials << RecipeMaterial.create(raw: milk, quantity: 0.6)
 
@@ -234,7 +234,7 @@ recipe_materials << RecipeMaterial.create(raw: pizza_dough, quantity: 175)
 
 recipe_materials << RecipeMaterial.create(raw: tomato_sauce, quantity: 0.65)
 
-pepperoni = Raw.create(name: 'Pepperoni', type: 'fresh', measure: 'gram', cost: '350')
+pepperoni = Raw.create(name: 'Pepperoni', type: 'meat', measure: 'gram', cost: '350')
 RawInventory.create(quantity: 500, expired_at: '2019-02-04', raw: pepperoni)
 recipe_materials << RecipeMaterial.create(raw: pepperoni, quantity: 175)
 
@@ -292,8 +292,64 @@ Product.create(recipe: recipe, expired_at: '2018-05-10')
 ####################### Recipe 18 #######################
 recipe_materials.clear # clear the array
 
+tortillas = Raw.create(name: 'Tortillas', type: 'miscellaneous', measure: 'kilogram', cost: '100')
+RawInventory.create(quantity: 2, expired_at: '2019-10-03', raw: tortillas)
+recipe_materials << RecipeMaterial.create(raw: tortillas, quantity: .025)
+
+carne_pastor = Raw.create(name: 'Carne al pastor', type: 'meat', measure: 'gram', cost: '440')
+RawInventory.create(quantity: 650, expired_at: '2019-07-06', raw: carne_pastor)
+recipe_materials << RecipeMaterial.create(raw: carne_pastor, quantity: 85)
+
+recipe_materials << RecipeMaterial.create(raw: salt, quantity: 5)
+
+recipe_materials << RecipeMaterial.create(raw: lemon, quantity: 1)
+
+recipe = Recipe.create(name: 'Tacos al Pastor', quantity: '130', measure: 'g', recipe_materials: recipe_materials)
+
+Product.create(recipe: recipe, expired_at: '2018-12-12')
+
 ####################### Recipe 19 #######################
 recipe_materials.clear # clear the array
 
+recipe_materials << RecipeMaterial.create(raw: tortillas, quantity: .025)
+
+recipe_materials << RecipeMaterial.create(raw: salt, quantity: 5)
+
+recipe_materials << RecipeMaterial.create(raw: lemon, quantity: 1)
+
+carne_bistec = Raw.create(name: 'Carne bistec', type: 'meat', measure: 'gram', cost: '540')
+RawInventory.create(quantity: 650, expired_at: '2019-07-06', raw: carne_bistec)
+recipe_materials << RecipeMaterial.create(raw: carne_bistec, quantity: 85)
+
+recipe = Recipe.create(name: 'Tacos de Bistec', quantity: '130', measure: 'g', recipe_materials: recipe_materials)
+
+Product.create(recipe: recipe, expired_at: '2018-11-11')
+
 ####################### Recipe 20 #######################
 recipe_materials.clear # clear the array
+
+flour = Raw.create(name: 'Flour', type: 'miscellaneous', measure: 'kilogram', cost: '200')
+RawInventory.create(quantity: 6, expired_at: '2018-05-01', raw: flour)
+recipe_materials << RecipeMaterial.create(raw: flour, quantity: 0.3)
+
+recipe_materials << RecipeMaterial.create(raw: salt, quantity: 15)
+
+recipe_materials << RecipeMaterial.create(raw: butter, quantity: 80)
+
+egg = Raw.create(name: 'Egg', type: 'fresh', measure: 'unit', cost: '10')
+RawInventory.create(quantity: 12, expired_at: '2018-05-02', raw: egg)
+recipe_materials << RecipeMaterial.create(raw: egg, quantity: 2)
+
+recipe_materials << RecipeMaterial.create(raw: milk, quantity: 0.75)
+
+vanilla = Raw.create(name: 'Vanilla', type: 'meat', measure: 'mililiters', cost: '450')
+RawInventory.create(quantity: 450, expired_at: '2018-05-01', raw: shrimp)
+recipe_materials << RecipeMaterial.create(raw: shrimp, quantity: 135)
+
+chocolate_chips = Raw.create(name: 'Chocolate Chips', type: 'fresh', measure: 'gram', cost: '1')
+RawInventory.create(quantity: 300, expired_at: '2018-12-01', raw: chocolate_chips)
+recipe_materials << RecipeMaterial.create(raw: chocolate_chips, quantity: 0.05)
+
+recipe = Recipe.create(name: 'Chocolate chip cookies', quantity: '250', measure: 'g', recipe_materials: recipe_materials)
+
+Product.create(recipe: recipe, expired_at: '2018-05-25')
